110ea9b8e8f90d158719338488099a24ddd2519a
MMPrado <maciaspradomayra@gmail.com>
2019-11-18 22:37:56 -0600

    Add files via upload

---
+++ main/java/com/example/loginsqlite/ItemUserRecicler.kt
@@ -0,0 +1,13 @@
+package com.example.loginsqlite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.loginsqlite.R
+
+class ItemUserRecicler : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.item_user_recicler)
+    }
+}
